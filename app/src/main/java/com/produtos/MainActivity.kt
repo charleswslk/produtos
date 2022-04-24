5c3df677020316076ef09bc07ea6b943c586266c
charleswslk <charles.wslk@gmail.com>
2022-04-24 13:30:51 -0300

    primeiro commit

---
+++ app/src/main/java/com/produtos/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.produtos
+
+import android.app.Activity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
